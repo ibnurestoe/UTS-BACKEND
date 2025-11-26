a3f175617b8dedf6e55470bb0716dfdd775cb3bb
ibnurestoe <ibnurestoe@students.amikom.ac.id>
2025-11-26 07:19:17 +0700

    web.php

--- web.php
+++ web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController; // Pastikan baris ini ada
+use App\Http\Controllers\ProfileController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,13 +12,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Middleware Auth
 Route::middleware('auth')->group(function () {
     
-    // --- Route untuk Soal Ujian (Produk) ---
     Route::resource('products', ProductController::class);
 
-    // --- Route Bawaan Profile (WAJIB ADA agar tidak error) ---
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
