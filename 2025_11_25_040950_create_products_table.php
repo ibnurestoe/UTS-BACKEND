d08f5ef6cd0a3db318ae050724de9e337c040ac5
ibnurestoe <ibnurestoe@students.amikom.ac.id>
2025-11-26 07:22:01 +0700

    2025_11_25_040950_create_products_table.php

--- 2025_11_25_040950_create_products_table.php
+++ 2025_11_25_040950_create_products_table.php
@@ -6,26 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
         $table->id();
-        // Sesuai aturan validasi: name max 100 char [cite: 39]
         $table->string('name', 100);
-        // Price > 0 (gunakan integer atau decimal/double) [cite: 40]
         $table->decimal('price', 15, 2);
-        // Stock integer [cite: 41]
         $table->integer('stock');
         $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
